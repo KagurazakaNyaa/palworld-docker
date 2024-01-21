9697849e9ace827677dda34efc94606013f3d30b
KagurazakaNyaa <dev@kagurazakanyaa.com>
2024-01-21 15:45:06 +0800

    fix force update Close #5

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if [[ -n $FORCE_UPDATE ]] && [[ $FORCE_UPDATE == "true" ]];then
-    steamcmd +force_install_dir "/opt/palworld" +login anonymous +app_update 2394010 validate +quit
+    /home/steam/steamcmd/steamcmd.sh +force_install_dir "/opt/palworld" +login anonymous +app_update 2394010 validate +quit
 fi
 
 if [[ ! -f /opt/palworld/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini ]];then
